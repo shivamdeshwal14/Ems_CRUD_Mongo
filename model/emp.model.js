81a7b4f3f5cb6b0821d81963bd79e9334b9ed426
SHIVAM DESHWAL <shivamdeshwal1414@gmail.com>
2023-02-20 03:32:13 +0530

    Add Emmployee Functionality adeed

--- model/emp.model.js
+++ model/emp.model.js
@@ -1,16 +1,18 @@
 const moongose=require('mongoose')
- var empschema=new moongose.Schema()
+ var empschema=new moongose.Schema({
  fullName:{
     type:String
- }
+ },
 email:{
     type:String
- }
+ },
 phone:{
     type:String
- }
+ },
  city:{
     type:String
- }
- var Employee1=moongose.model('Employee',empschema);
- module.exports=Employee1
+ },
+ });
+ module.exports =moongose.model('Employee',empschema);
+
+ 
